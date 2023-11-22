9429177bdcab7648a43c97c26d08cdfdd4f30cb2
Martin Rosas <mlrosas21@hotmail.com>
2023-11-22 10:41:34 -0300

    endpoint always 200

--- src/pages/api/prueba/index.ts
+++ src/pages/api/prueba/index.ts
@@ -5,22 +5,22 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
-    const { id } = req.query;
-    console.log(id)
-    const url = `${process.env.PRODUCT_API_URL}/fav/user/36`;
-    console.log(url)
-    const response = await fetch(url);
-    console.log(response)
-    let data;
-    if (response.ok) {
-      data = await response.json();
-    } else {
-      data = [];
-    }
+    // const { id } = req.query;
+    // console.log(id)
+    // const url = `${process.env.PRODUCT_API_URL}/fav/user/36`;
+    // console.log(url)
+    // const response = await fetch(url);
+    // console.log(response)
+    // let data;
+    // if (response.ok) {
+    //   data = await response.json();
+    // } else {
+    //   data = [];
+    // }
 
-    console.log(data)
+    // console.log(data)
 
-    res.status(200).json(data);
+    res.status(200).json([]);
   }
   if (req.method === "POST") {
     res.status(200).json("asda");
